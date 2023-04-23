f6505a91b5bc9c4a0c167bea533bb54b479e3a6a
mohamedamor002 <mohamed.amor@esprit.tn>
2023-04-23 11:17:46 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
-
 #include "mainwindow.h"
+#include <QMessageBox>
+#include "connection.h"
 
 #include <QApplication>
-
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    connection c  ;
+    bool test=c.createConnection() ;
     MainWindow w;
-    w.show();
+    if(test)
+    {w.show();
+        QMessageBox::information(nullptr,QObject::tr("Database is open"),
+                                 QObject::tr("Connection successful.\n Click cancel to exist"),QMessageBox::Cancel);
+
+    }
+    else
+        QMessageBox::information(nullptr,QObject::tr("Database is not open"),
+                                 QObject::tr("Connection failed.\n Click cancel to exist"),QMessageBox::Cancel);
+
+
     return a.exec();
 }
